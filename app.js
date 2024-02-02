78141e5d2d415ce751f64c2d931026939c357591
sebas <sebas@home.com>
2024-02-02 03:31:38 -0600

    Agregar bosquejos de estilos a pagina principal y funcion de presentar saldo individual

--- app.js
+++ app.js
@@ -10,18 +10,57 @@ const db = new pg.Client({
     port: conf.port,
     database: conf.database,
     user: conf.user,
-    password:conf.password
+    password: conf.password
 });
 db.connect();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+let currentUserId = 0;
+var saldoIndividual = 0;
+
+async function sumarSaldoIndividual() {
+    try {
+        let response = await db.query(
+            "SELECT cuentas.saldo "+
+            "FROM cuentas "+
+            "JOIN usuarios "+
+            "ON id_usuario = usuarios.id "+
+            "WHERE usuarios.id = $1;",
+            [currentUserId]
+        );
+        let sum = 0;
+        response.rows.forEach(saldo => {
+            sum+=parseFloat(saldo.saldo);
+        });
+        return sum;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 app.get("/", async (req,res)=>{
-    const response = await db.query("SELECT * FROM usuarios WHERE id=1");
-    const result = response.rows[0].nombre;
-    console.log(result);
-    res.render("index.ejs", {data:result});
+    try {
+        const users = await db.query(
+            "SELECT * FROM usuarios;"
+        );
+        const result = users.rows;
+        res.render("index.ejs", {
+            users: result,
+            currentUser: currentUserId, 
+            infoSaldo: saldoIndividual
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.render("index.ejs",{users:error.message});
+    }
+});
+
+app.post("/user", async (req,res) => {
+    currentUserId = req.body.user;
+    saldoIndividual = await sumarSaldoIndividual();
+    res.redirect("/");
 });
 
 app.listen(port, () => {
