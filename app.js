11dcd6eab9a505fecacb496bfc9ca538b2a33048
sebas <sebas@home.com>
2024-02-04 03:47:13 -0600

    Se agrega formulario de ingresos y alteracion en base de datos

--- app.js
+++ app.js
@@ -18,27 +18,54 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 let currentUserId = 0;
-var saldoIndividual = 0;
+let currentUserInfo = [];
+let saldoIndividual = 0;
 
-async function sumarSaldoIndividual() {
+async function userInfo() {
+    saldoIndividual = 0;
     try {
         let response = await db.query(
-            "SELECT cuentas.saldo "+
-            "FROM cuentas "+
-            "JOIN usuarios "+
-            "ON id_usuario = usuarios.id "+
-            "WHERE usuarios.id = $1;",
+            "SELECT usuarios.nombre, cuentas.id as cuenta_id, "+
+            "cuentas.nombre as cuenta_nombre, cuentas.saldo "+
+            "FROM usuarios "+
+            "JOIN cuentas "+
+            "ON usuarios.id = cuentas.id_usuario "+
+            "WHERE usuarios.id = $1 "+
+            "ORDER BY cuenta_id",
             [currentUserId]
         );
-        let sum = 0;
-        response.rows.forEach(saldo => {
-            sum+=parseFloat(saldo.saldo);
+        response.rows.forEach(cuenta => {
+            saldoIndividual+=parseFloat(cuenta.saldo);
         });
-        return sum;
+        return response.rows;
     } catch (error) {
         console.log(error.message);
     }
 }
+
+async function insertMovimiento(req,res,next){
+    if(req.body.tipo) {
+        console.log(req.body);
+        let data = [
+            req.body.tipo,req.body.categoria,
+            parseFloat(req.body.importe),req.body.fecha,
+            req.body.observacion,currentUserId,
+            parseInt(req.body.cuenta)
+        ];
+        console.log(data);
+        try {
+            let upMov = await db.query(
+                "INSERT INTO movimientos (tipo,categoria,importe,fecha,observacion,id_usuario,id_cuenta) VALUES ($1,$2,$3,$4,$5,$6,$7);",
+                data
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    next();
+};
+
+app.use(insertMovimiento);
 
 app.get("/", async (req,res)=>{
     try {
@@ -49,7 +76,8 @@ app.get("/", async (req,res)=>{
         res.render("index.ejs", {
             users: result,
             currentUser: currentUserId, 
-            infoSaldo: saldoIndividual
+            infoSaldo: saldoIndividual,
+            cuentas: currentUserInfo
         });
     } catch (error) {
         console.log(error.message);
@@ -59,8 +87,34 @@ app.get("/", async (req,res)=>{
 
 app.post("/user", async (req,res) => {
     currentUserId = req.body.user;
-    saldoIndividual = await sumarSaldoIndividual();
+    currentUserInfo = await userInfo();
     res.redirect("/");
+});
+
+app.post("/ingresos", async (req,res) => {
+    let importe = parseFloat(req.body.importe);
+    let cuenta = req.body.cuenta;
+    try {
+        let quSaldo = await db.query(
+            "SELECT saldo "+
+            "FROM cuentas "+
+            "WHERE id=$1",
+            [cuenta]
+        );
+        let antiguoSaldo = parseFloat(quSaldo.rows[0].saldo);
+        let nuevoSaldo = importe+antiguoSaldo;
+        
+        let upSaldo = await db.query(
+            "UPDATE cuentas "+
+            "SET saldo = $1 "+
+            "WHERE id = $2;",
+            [importe,cuenta]
+        );
+        res.redirect("/");
+        
+    } catch (error) {
+      console.log(error.message);  
+    } 
 });
 
 app.listen(port, () => {
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,11 @@
-$("button").click(()=>{
+$("#push").click(()=>{
     $("#navBar").slideToggle();
+});
+
+$("#ingresosBtn").click(()=> {
+    if($("#ingresos").attr("hidden")) {
+        $("#ingresos").removeAttr("hidden");
+    } else {
+        $("#ingresos").attr("hidden","hidden");
+    }
 });
